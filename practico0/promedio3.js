f9d50f23d2e290131dd620788b33b947971f7eed
lomejorweb <lomejorweb.23@gmail.com>
2023-05-22 01:21:47 -0300

    Practico N0 finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,3 @@
+console.log ("el promedio entre 5,7,9 es = " + (5+7+9)/3);
+console.log ("el promedio entre -2,-3,-8 es = " + ((-2)+(-3)+(-8))/3);
+console.log ("el promedio entre -1.2,5.3,-3.3 es = " + ((-1.2)+5.3+(-3.3))/3);
